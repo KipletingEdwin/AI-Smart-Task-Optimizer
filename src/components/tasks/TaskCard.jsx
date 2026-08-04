4d3bfc147132fe96566df35523881b0c794e2827
KipletingEdwin <kipletingedwin4@gmail.com>
2026-08-04 15:41:30 +0100

    commit

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,8 +1,20 @@
 
 import React from 'react'
 
-export const TaskCard = () => {
+  const priorityStyles = {
+    low: "bg-neutral-100 text-neutral-600",
+    medium: "bg-amber-100 text-amber-700",
+    high: "bg-rose-100 text-rose-700",
+  };
+
+export const TaskCard = ({task, onToggleSub, onDelete}) => {
+  
+
+
   return (
     <div>TaskCard</div>
   )
 }
+
+
+
