f4efb4ecc0405d23c81705839ca26a681f7db4bc
KipletingEdwin <kipletingedwin4@gmail.com>
2026-08-04 15:43:18 +0100

    commit

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -8,6 +8,18 @@ import React from 'react'
   };
 
 export const TaskCard = ({task, onToggleSub, onDelete}) => {
+
+  const subtasks = task.subtasks || [];
+  const doneCount = subtasks.filter((s) => s.done).length;
+  
+
+
+
+
+
+  //   const subtasks = task.subtasks || [];
+  // const doneCount = subtasks.filter((s) => s.done).length;
+  // const progress = subtasks.length ? (doneCount / subtasks.length) * 100 : 0;
   
 
 
