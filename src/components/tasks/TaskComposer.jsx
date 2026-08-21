a8a51d79ed643bcb39ec9508455ba904d085095d
KipletingEdwin <kipletingedwin4@gmail.com>
2026-08-21 21:48:55 +0100

    commit

--- src/components/tasks/TaskComposer.jsx
+++ src/components/tasks/TaskComposer.jsx
@@ -36,7 +36,7 @@ export const TaskComposer = ({ onGenerate, loading }) => {
         <Button
         onClick={submit} disabled={loading || !text.trim()}
         className="rounded-full px-6" />
-                { loading ? <Loader2 className='mr-2 h-4 w-4 animate-spin'/> : <Sparkles className='mr-2  '/>   }
+                { loading ? <Loader2 className='mr-2 h-4 w-4 animate-spin'/> : <Sparkles className='mr-2 h-4 w-4'/>}
 
 
       </div>
